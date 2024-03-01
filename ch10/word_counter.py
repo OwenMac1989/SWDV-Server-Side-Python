b1fc953fdb21658e7f45b6a9fc1a531d4ae92c0f
OwenMac1989 <tycreager@gmail.com>
2024-02-29 22:42:29 -0700

    lab 10.2

--- ch10/word_counter.py
+++ ch10/word_counter.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+def get_sentences_from_file(filename):
+    with open(filename) as file:
+        text = file.read()    # read str from file
+ 
+    sentences = text.split(".")   # convert str to list
+    sentences.sort()
+    # print(words)
+    return sentences
+
 def get_words_from_file(filename):
     with open(filename) as file:
         text = file.read()    # read str from file
@@ -31,12 +40,15 @@ def main():
     print("The Word Counter program\n")  
 
     # get words and unique words
+    sentences = get_sentences_from_file(filename)
     words = get_words_from_file(filename) # get list of words
     unique_words = get_unique_words(words)
 
-    # display number of words and unique words   
+    # display number of words and unique words 
+    print(f"Number of sentences = {len(sentences)}")  
     print(f"Number of words = {len(words)}")
     print(f"Number of unique words = {len(unique_words)}")
+
 
     # display unique words and their word counts
     print("Unique word occurrences:")
