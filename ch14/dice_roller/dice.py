6ceafb3d6f0c2ddd7f0a7903bf7a17eb94951fb8
OwenMac1989 <tycreager@gmail.com>
2024-03-30 18:40:14 -0600

    Lab 14-1

--- ch14/dice_roller/dice.py
+++ ch14/dice_roller/dice.py
@@ -4,20 +4,60 @@ from dataclasses import dataclass
 @dataclass
 class Die:
     __value:int = 1
+ 
+    def __post_init__(self):
+        self.__value = self.roll()
 
     @property
     def value(self):
         return self.__value
-                
+
     @value.setter
     def value(self, value):
-        if value < 1:
-            raise ValueError("Die value can't be less than 1.")
+        if value < 1 or value > 6:
+            raise ValueError("Die value can't be less than 1 or greater than 6.")
+
         else:
             self.__value = value
-                
+
+    @property
+    def image(self):
+        if self.__value == 6:
+            return " _____ \n" + \
+                    "|o   o|\n" + \
+                    "|o   o|\n" + \
+                    "|o___o|"
+        elif self.__value == 5:
+            return " _____ \n" + \
+                   "|o   o|\n" + \
+                   "|  o  |\n" + \
+                   "|o___o|"
+        elif self.__value == 4:
+            return " _____ \n" + \
+                   "|o   o|\n" + \
+                   "|     |\n" + \
+                   "|o___o|"
+        elif self.__value == 3:
+            return " _____ \n" + \
+                   "|o    |\n" + \
+                   "|  o  |\n" + \
+                   "|____o|"
+        elif self.__value == 2:
+            return " _____ \n" + \
+                   "|o    |\n" + \
+                   "|     |\n" + \
+                   "|____o|"
+        elif self.__value == 1:
+            return " _____ \n" + \
+                   "|     |\n" + \
+                   "|  o  |\n" + \
+                   "|_____|"
+        return self.image
+    
+ 
     def roll(self):
         self.__value = random.randrange(1, 7)
+        return self.__value
 
                 
 class Dice:
@@ -36,3 +76,10 @@ class Dice:
     def rollAll(self):
         for die in self.__list:
             die.roll()
+
+    def getTotal(self):
+        total = 0
+        for die in self.__list:
+            total += die.value
+        return total
+            
